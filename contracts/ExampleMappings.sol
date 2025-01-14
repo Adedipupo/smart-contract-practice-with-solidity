87766b43899661aa73c9c0933b1f640117256e75
Adedipupo <jokanolaadedipupo@gmail.com>
2025-01-14 08:02:04 +0100

    mappings

---
+++ contracts/ExampleMappings.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+contract Example {
+
+    mapping (uint => bool) public myMapping;
+    mapping (address => bool) public myAddressMapping;
+    mapping (uint => mapping (uint => bool)) public unitUintBoolMapping;
+
+    function setValue(uint _index)public {
+        myMapping[_index] = true;
+    }
+
+    function setAddressToTrue() public {
+        myAddressMapping[msg.sender] = true;
+    }
+
+    function setUintUintBoolMapping(uint _key1,uint _key2,bool _value) public {
+        unitUintBoolMapping[_key1][_key2] = _value;
+    }
+}
